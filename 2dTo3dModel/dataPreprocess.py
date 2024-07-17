e68c777d9eb88dd3987288397d8adf93d4d1a3b4
yluoc <98978059+yluoc@users.noreply.github.com>
2024-07-16 22:16:49 -0400

    Update dataPreprocess.py

--- 2dTo3dModel/dataPreprocess.py
+++ 2dTo3dModel/dataPreprocess.py
@@ -1,112 +1,25 @@
-import os
 import cv2
-import trimesh
 import numpy as np
-import pandas as pd
-import importlib.util
-from PIL import Image
+import os
 
-# function to import model from generator files
-def import_model_from_outside(model_name, filepath):
-    spec = importlib.util.spec_from_file_location(model_name, filepath)
-    model = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(model)
-    return model
-
-file_path1 = os.path.join(os.path.dirname(__file__), '2DShapeGenerator.py')
-file_path2 = os.path.join(os.path.dirname(__file__), '3DShapeGenerator.py')
-
-img_generator_model = import_model_from_outside('2DShapeGenerator', file_path1)
-mdl_generator_model = import_model_from_outside('3DShapeGenerator', file_path2)
-
-class dataPreprocess():
-    def __init__(self, image_path, model_path, obj_num=0):
-        self.image_path = image_path
-        self.model_path = model_path
+class dataPreprocess:
+    def __init__(self, img_path, mdl_path, obj_num):
+        self.img_path = img_path
+        self.mdl_path = mdl_path
         self.obj_num = obj_num
 
-    # function to match related shape to model
     def shape_model_match(self):
-        color_RGB = img_generator_model.random_color_tuple()
-        img_generator_model.image_generator(self.obj_num, color_RGB)
-        mdl_generator_model.model_generator(self.obj_num, color_RGB)
-        shape_model_map = {'triangle': 'pyramid', 'square': 'cube', 'circle': 'sphere'}
-        shape_info = pd.read_csv(self.image_path+'/shape_colors.csv')
-        model_info = pd.read_csv(self.model_path+'/shape_colors.csv')
+        img_files = sorted(os.listdir(self.img_path))[:self.obj_num]
+        mdl_files = sorted(os.listdir(self.mdl_path))[:self.obj_num]
 
-        shape_list = shape_info.values.tolist()
-        model_list = model_info.values.tolist()
+        dataset = []
+        for img_file, mdl_file in zip(img_files, mdl_files):
+            dataset.append((os.path.join(self.img_path, img_file), os.path.join(self.mdl_path, mdl_file), img_file, mdl_file))
+        return dataset
 
-        matching_result = []  # shape_path, model_path, model, shape, color
-        for shape in shape_list:
-            for model in model_list:
-                if shape[2] == model[2] and shape_model_map.get(shape[1]) == model[1]:
-                    matching_result.append([shape[0], model[0], shape[1], model[1], shape[2]])
-
-        return matching_result
-    
-    # function to preprocess image(resize, normalize)
-    def image_preprocess(self, img_filedir):
-        imgs = []
-        for filename in os.listdir(img_filedir):
-            if filename.endswith('.png'):
-                img = cv2.imread(os.path.join(img_filedir, filename))
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                img = cv2.resize(img, (128, 128))
-                img = img = img/255.0
-                imgs.append(img)
-        return np.array(imgs)
-    
-    # function to rotate img
-    def rotate_img(self, img, angle):
-        h, w = img.shape[:2]
-        M = cv2.getRotationMatrix2D((w/2, h/2), angle, 1)
-        return cv2.warpAffine(img, M, (w, h))
-
-    # function to flip img
-    def filp_img(self, img, flip_code):
-        return cv2.flip(img, flip_code)
-
-    # function to scale img
-    def scale_img(self, img, scale_factor):
-        h, w = img.shape[:2]
-        return cv2.resize(img, (int(w*scale_factor), int(h*scale_factor)))
-
-    # function to augment imgs
-    def augment_imgs(self, imgs):
-        augmented_imgs = []
-        for img in imgs:
-            augmented_imgs.append(img)
-
-            for angle in [15, -15]:
-                augmented_imgs.append(self.rotate_img(img, angle))
-
-            for flip_code in [0, 1]:
-                augmented_imgs.append(self.filp_img(img, flip_code))
-            
-            for scale_factor in [0.8, 1.2]:
-                augmented_imgs.append(self.scale_img(img, scale_factor))
-        
-        return np.aray(augmented_imgs)
-
-    # function to preprocess model
-    def model_preprocess(self, mdl_filedir):
-        models = []
-        for filename in os.listdir(mdl_filedir):
-            if filename.endswith('.obj'):
-                mesh = trimesh.load(os.path.join(mdl_filedir, filename))
-                
-                # normalize the model
-                mesh.apply_translation(-mesh.centroid)
-                mesh.apply_scale(1/mesh.scale)
-                
-                # convert to voxel grid
-                voxel_grid = mesh.voxelized(pitch=0.1).filled()
-
-                voxel_grid = np.pad(voxel_grid, ((0, 32 - voxel_grid.shape[0]),
-                                                 (0, 32 - voxel_grid.shape[1]),
-                                                 (0, 32 - voxel_grid.shape[2])), 'constant')
-                
-                models.append(voxel_grid)
-            
-            return np.array(models)
+    def image_preprocess(self, img_filepath):
+        img = cv2.imread(img_filepath)
+        if img is not None:
+            img = cv2.resize(img, (128, 128))  # Resize images to a fixed size
+            img = img / 255.0  # Normalize the image
+        return img
